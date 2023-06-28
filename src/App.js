8c22610031c2d6ba260d906415cad438da28aaa1
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2023-06-28 13:09:10 +0530

    created pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const fetchProducts = async () => {
+    const response = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await response.json();
+
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  };
+
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 &&
+      selectedPage !== page
+    )
+      setPage(selectedPage);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="max-h-screen mt-5 px-8 md:px-16 ">
+        {products.length > 0 && (
+          <div className="grid grid-cols-1 py-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 2xl:grid-cols-4">
+            {products.slice(page * 10 - 10, page * 10).map((product) => (
+              <div
+                className="bg-neutral-100 rounded-md p-3 m-3 hover:scale-105 cursor-pointer transition "
+                key={product.id}
+              >
+                <h2 className="text-lg py-1 font-bold text-red-900">
+                  {product.title}
+                </h2>
+                <img
+                  src={product.thumbnail}
+                  alt={product.title}
+                  className="product__img p-2  rounded-md"
+                />
+                <div className="flex flex-row items-center justify-between">
+                  <span className="text-slate-400 py-2">
+                    {product.discountPercentage} % Offer
+                  </span>
+                  <span className="text-slate-700 py-2">₹ {product.price}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+
+        <div className="flex flex-col  items-center justify-center mx-auto py-5">
+          {products.length > 0 && (
+            <nav>
+              <ul className="inline-flex -space-x-px">
+                <li>
+                  <span
+                    onClick={() => selectPageHandler(page - 1)}
+                    className={`px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-slate-500 hover:text-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer ${
+                      page > 1
+                        ? " "
+                        : "cursor-not-allowed hover:bg-gray-50 hover:text-red-200"
+                    }`}
+                  >
+                    Previous
+                  </span>
+                </li>
+
+                <li>
+                  {[...Array(products.length / 10)].map((_, i) => {
+                    return (
+                      <span
+                        onClick={() => selectPageHandler(i + 1)}
+                        className={` px-3 py-2 leading-tight cursor-pointer text-gray-500 bg-white border border-gray-300 hover:bg-slate-500 hover:text-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white  ${
+                          page === i + 1
+                            ? "bg-slate-500 border-1 border-slate-500 text-white"
+                            : ""
+                        }`}
+                      >
+                        {i + 1}
+                      </span>
+                    );
+                  })}
+                </li>
+                <li>
+                  <span
+                    onClick={() => selectPageHandler(page + 1)}
+                    className={`px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-slate-500 hover:text-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer ${
+                      page > products.length / 10
+                        ? " "
+                        : "cursor-not-allowed hover:bg-gray-50 hover:text-red-200"
+                    }`}
+                  >
+                    Next
+                  </span>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import { FiLogIn, FiShoppingCart } from "react-icons/fi";
 
 const Header = () => {
   return (
-    <header className="sticky w-full h-40 border-b border-gray-400 shadow-lg">
-      <div className="p-8 md:p-16 py-2 flex flex-row items-center justify-between">
-        <div>logo</div>
-        <div>Search</div>
-        <div>Account</div>
+    <header className="w-full py-3 border-b border-gray-300 shadow-md">
+      <div className="px-8 md:px-16   flex flex-row items-center justify-between">
+        <div className="flex flex-row gap-2 items-center">
+          <img
+            src={"./images/online-shop.png"}
+            alt="logo"
+            className="w-[30px] md:w-[40px]"
+          />
+          <span className="text-gray-700 font-extralight text-2xl md:text-3xl">
+            Shopping Kart
+          </span>
+        </div>
+        <div className="hidden md:flex">
+          <input
+            type="search"
+            className="border border-gray-200 outline-none focus:outline-1 w-[340px] px-3 py-2 rounded-md "
+            placeholder="Search something..."
+          />
+        </div>
+        <div className="flex flex-row items-center gap-8">
+          <span className="flex flex-row items-center gap-1 cursor-pointer">
+            <FiLogIn size={24} /> Sign In
+          </span>
+          <span className="flex flex-row items-center cursor-pointer">
+            <FiShoppingCart size={24} />
+          </span>
+        </div>
       </div>
     </header>
   );
